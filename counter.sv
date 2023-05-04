0a20cb30faceec16b10f02ee72c30aa880a2d355
Hassan khaled <hassankhaled006@gmail.com>
2023-05-04 06:26:21 +0300

    Counter Final version

--- counter.sv
+++ counter.sv
@@ -1,41 +1,43 @@
-module multi_mode_counter(
+module multi_mode_counter #(parameter WIDTH)(
 
-input            clk          ,
-input            rst_n        ,
-input            init         ,
-input      [3:0] load_value   ,
-input      [1:0] mode_control ,
+input            clk                ,
+input            rst_n              ,
+input            init         	    ,
+input      [WIDTH-1:0] load_value   ,
+input      [1:0] mode_control       ,
 
-output reg       GAMEOVER     ,
+output reg       GAMEOVER           ,
 output reg [1:0] WHO
 
 );
 
+wire [WIDTH-1:0] max_value;
 
+assign max_value = (1 << WIDTH) - 1;
 //------ DECLARATIONS ---------------------
 
 typedef enum reg[2:0] {INITIAL = 3'b000,COUNT = 3'b001,WINNER_FLAG = 3'b010 , LOSER_FLAG = 3'b011 , COMPLETE = 3'b100} state;
 state current_state,next_state;
 
+wire WINNER;
+wire LOSER;
 
-reg WINNER;
-reg LOSER;
 
-
-reg unsigned[3:0] counter;
+reg unsigned[WIDTH-1:0] counter;
 reg [3:0] winner_counter,loser_counter;
 
 
 
-assign WINNER = (counter == 4'd15 && current_state == 3'b001)? 1'b1 : 0;
-assign LOSER =  (counter == 4'd00 && current_state == 3'b001)? 1'b1 : 0;
+assign WINNER = ( (&counter) && (current_state == 3'b001))? 1'b1 : 0;
+assign LOSER =  (counter == 0 && current_state == 3'b001)? 1'b1 : 0;
+
 
 //-------------------------------------------------------------------
 
 always @(posedge clk or negedge rst_n) begin
 	
 	if(~rst_n) begin
-		current_state <= INITIAL;
+          current_state <= INITIAL;
 	end 
 
 	else begin
@@ -83,23 +85,18 @@ end
 
 //-------------------------------------------------------------------
 
-
 always @(posedge clk)begin
 
    case(current_state)
    	
     INITIAL : begin
 
-              counter <= 4'b1;              
+              counter <= 0;              
               GAMEOVER <= 0;
               WHO <= 0;
-              //WINNER <= 0;
-	     // LOSER <= 0;
 	      winner_counter <=0;
               loser_counter <= 0;
-
     	      end
-
 
 
    COUNT : begin
@@ -109,9 +106,9 @@ always @(posedge clk)begin
            end
 
 
-       	    else if (mode_control == 2'b00)begin
-	      counter <= counter +  1 ;
-	    end
+       	  else if (mode_control == 2'b00)begin
+	     counter <= counter +  1 ;
+	   end
 
 
 	 else if(mode_control == 2'b01)begin
@@ -131,16 +128,13 @@ always @(posedge clk)begin
    end
 
 
-
     WINNER_FLAG: begin
-        	//WINNER = 0;
         	winner_counter = winner_counter + 1;
          	 end
 
 
     LOSER_FLAG: begin
-                  // LOSER = 0;
-                   loser_counter = loser_counter + 1;
+                loser_counter = loser_counter + 1;
                 end     
 
 
@@ -155,6 +149,56 @@ always @(posedge clk)begin
     endcase
 
 end
+
+
+property winner_as;
+@(posedge clk) disable iff(!rst_n || GAMEOVER ) (&counter) |-> ##[0:1] $rose(WINNER);
+endproperty
+
+
+winner_assert: assert property(winner_as);
+winner_assert_cov: cover property(winner_as);
+
+
+property winner_deass;
+@(posedge clk) disable iff(!rst_n || GAMEOVER ) $rose(WINNER) |=>  $fell(WINNER);
+endproperty
+
+winner_deassert: assert property(winner_deass);
+winner_deassert_cov: cover property(winner_deass);
+
+
+property loser_as;
+@(posedge clk) disable iff(!rst_n || GAMEOVER ) !(counter) |-> ##[0:1] $rose(LOSER);
+endproperty
+
+loser_assert: assert property(loser_as);
+loser_assert_cov: cover property(loser_as);
+
+
+property loser_deass;
+@(posedge clk) disable iff(!rst_n || GAMEOVER ) $rose(LOSER) |=>  $fell(LOSER);
+endproperty
+
+loser_deassert: assert property(loser_deass);
+loser_deassert_cov: cover property(loser_deass);
+
+
+property gameover_check;
+@(posedge clk) (winner_counter == 4'b1111 || loser_counter == 4'b1111) |-> ##[1:2] $rose(GAMEOVER);
+endproperty
+
+assert property(gameover_check);
+cover property (gameover_check);
+
+
+
+property WHO_check;
+@(posedge clk) $rose(GAMEOVER) |-> (WHO == 2'd2||WHO == 2'd1);
+endproperty
+
+assert property(WHO_check);
+cover property (WHO_check);
 
 
 
--- counter_tb.sv
+++ counter_tb.sv
@@ -1,16 +1,88 @@
+parameter WIDTH = 'd100;
+
+class C_Class;
+
+rand bit init;
+rand bit [1:0] mode_control;
+rand logic [WIDTH-1:0] load_value;
+bit winner_testcase;
+bit loser_testcase; 
+
+//============== CONSTRAINS ====================
+
+constraint init_c{
+        (winner_testcase == 1 || loser_testcase == 1) ->  init == 0 ;
+        init dist {1:/1 , 0:/9};
+}
+
+constraint ctrl_c{
+        (winner_testcase == 1) ->  mode_control dist{2'b00:/50 , 2'b01:/50};
+        (loser_testcase == 1) ->  mode_control dist{2'b10:/50 , 2'b11:/50};
+         mode_control dist {2'b00:/25 , 2'b01:/25 , 2'b10:/25 , 2'b11:/25};
+}
+
+
+//============== COVER GROUP ====================
+
+
+covergroup cg;
+
+ctrl_bits: coverpoint mode_control {
+
+bins zero_zero = {2'b00};
+bins zero_one  = {2'b01};
+bins one_zero  = {2'b10};
+bins one_one   = {2'b11};
+
+bins zz_zo     = (2'b00 => 2'b01);
+bins zz_oz     = (2'b00 => 2'b10);
+bins zz_oo     = (2'b00 => 2'b11);
+
+bins zo_zz     = (2'b01 => 2'b00);
+bins zo_oz     = (2'b01 => 2'b10);
+bins zo_oo     = (2'b01 => 2'b11);
+
+bins oz_zz     = (2'b10 => 2'b00);
+bins oz_zo     = (2'b10 => 2'b01);
+bins oz_oo     = (2'b10 => 2'b11);
+
+bins oo_zz     = (2'b11 => 2'b00);
+bins oo_zo     = (2'b11 => 2'b01);
+bins oo_oz     = (2'b11 => 2'b10);
+}
+
+endgroup
+  
+  
+  
+
+function new();
+cg = new();
+endfunction
+
+endclass
+
+//----------------- TESTBENCH -------------------
+
+
 module multi_mode_counter_tb();
 
 reg         clk,rst_n,init ; 
-logic [3:0] load_value_tb  ;
+logic [WIDTH-1:0] load_value_tb  ;
 logic [1:0] mode_control_tb;
 
 wire  GAMEOVER;
 wire  [1:0] WHO; 
+wire [WIDTH-1:0] max_value;
 
 
-multi_mode_counter  counter_instance(.clk(clk),.rst_n(rst_n),.load_value(load_value_tb),.mode_control(mode_control_tb),
+multi_mode_counter   #(.WIDTH(WIDTH)) counter_instance (.clk(clk),.rst_n(rst_n),.load_value(load_value_tb),.mode_control(mode_control_tb),
                                      .init(init),.GAMEOVER(GAMEOVER) ,.WHO(WHO));
 
+
+C_Class cc_obj = new();
+
+assign max_value =(1 << WIDTH) - 1;
 
 initial begin
 clk = 0;
@@ -21,48 +93,70 @@ end
 
 
 initial begin
-init = 0;
-load_value_tb = 0;
-mode_control_tb = 0;
 rst_n = 0;
-#100;
+#2;
 rst_n = 1;
-#5;
+#2;
 
-mode_control_tb = 2'b0;
-#10;
-mode_control_tb = 2'b01;
-#6;
-mode_control_tb = 2'b10;
-#6;
-mode_control_tb = 2'b11;
-#6;
-load_value_tb = 4'd14;
+
+for(int i = 0 ; i <100 ; i++) begin
+@(negedge clk);
+assert(cc_obj.randomize());
+mode_control_tb = cc_obj.mode_control;
+init = cc_obj.init;
+load_value_tb = cc_obj.load_value;
+end
+rst_n = 0;
 #2;
-init=1;
+rst_n =1;
 #2;
-init = 0;
+
+winner_gameover();
+
+rst_n = 0;
 #2;
-mode_control_tb = 2'b11;
+rst_n =1;
+#2;
+
+loser_gameover();
+#2;
+end
+
+//========== TASK FOR TESTING GAMEOVER WITH WINNER ==============
+  
+task winner_gameover();
+init = 1;
+for(int i = 0 ; i < 20 ; i++)begin
+@(negedge clk);
+load_value_tb = max_value;
 #4;
-rst_n = 0;
+load_value_tb = 0;
+end
+init = 0;
+endtask
+
+//========== TASK FOR TESTING GAMEOVER WITH LOSER ===============
+
+task loser_gameover();
+init = 1;
+for(int i = 0 ; i < 20 ; i++)begin
+@(negedge clk);
+load_value_tb = 0;
+#4;
+load_value_tb = max_value;
+end
+init = 0;
 #2;
-rst_n = 1;
-mode_control_tb = 2'b0;
-end
-
-initial begin
-#690;
-mode_control_tb = 2;
-end
-
-
-initial begin
-#1215;
 $stop;
+endtask
+  
+//========== SAMPLING COVER GROUP ===============================
+  
+always @(posedge clk) begin
+cc_obj.cg.sample();
 end
-
 
 
 
 endmodule
+
